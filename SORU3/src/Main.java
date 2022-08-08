af8cf200861100c35b50ef6b7266666693bb5466
psyib <psyib@nottingham.ac.uk>
2022-08-08 16:08:31 +0300

    soru3

---
+++ SORU3/src/Main.java
@@ -0,0 +1,12 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		CustomStringTR ifade = new CustomStringTR();
+
+		ifade.ifade = "Hello world!  ";
+
+		System.out.println(ifade.bosluklariAl());
+	}
+}
